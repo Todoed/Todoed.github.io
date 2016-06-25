95d5b3addcc946ea790d838e38710ca1f5f89092
Dhaval <dhaval@dr4cun0.com>
2016-06-25 14:46:56 -0400

    ios

---
+++ ios/demo/js/min/scroll-min.js
@@ -0,0 +1,1 @@
+var className,doc,elm;className="scroll",elm=$("nav"),doc=$(document),doc.scroll(function(){return elm.toggleClass(className,doc.scrollTop()>=20)}),$(document).ready(function(){$(".logo").click(function(){$("body").animate({scrollTop:$("body").offset().top},900)})});
